21d34d561fd662e4a14a50b9a414482ef563c47b
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-03-01 17:24:49 +0500

    Time: 56 ms (81.54%), Space: 50.7 MB (32.59%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let obj = {}
+    
+    for (let i = 0;i<nums.length;i++){
+        if (obj[nums[i]] != undefined)
+            return [obj[nums[i]], i]
+        else 
+            obj[target - nums[i]] = i
+    }
+    
+    
+};
