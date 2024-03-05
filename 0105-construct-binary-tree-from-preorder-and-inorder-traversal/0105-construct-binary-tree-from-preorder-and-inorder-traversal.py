b1a4d5814894425a1ebee59378ba792bb7ca2333
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-03-05 21:28:53 +0500

    Time: 111 ms (67.25%), Space: 52.9 MB (63.92%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,12 +6,12 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if len(preorder) == 0 or len(inorder) == 0:
+        if not preorder or not inorder:
             return None
         
-        root_val = preorder.pop(0)
-        root_index = inorder.index(root_val)
-        root = TreeNode(root_val)
-        root.left = self.buildTree(preorder, inorder[:root_index])
-        root.right = self.buildTree(preorder, inorder[root_index+1:])
+        val = preorder.pop(0)
+        index = inorder.index(val)
+        root = TreeNode(val)
+        root.left = self.buildTree(preorder, inorder[:index])
+        root.right = self.buildTree(preorder, inorder[index + 1:])
         return root
