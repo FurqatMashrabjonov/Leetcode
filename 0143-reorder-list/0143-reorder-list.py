26b90370b75b620ba6288ca8314b28d6f2a830d2
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-03-09 21:44:51 +0500

    Time: 52 ms (76.83%), Space: 24.8 MB (28.62%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -6,27 +6,27 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         slow, fast = head, head
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
+            
         second = slow.next
         slow.next = None
-
+        
         prev = None
+        
         while second:
             tmp = second.next
             second.next = prev
             prev = second
             second = tmp
-        
-        first = head
         second = prev
-        
+        first = head
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
+            
             first, second = tmp1, tmp2
         
-        return head
