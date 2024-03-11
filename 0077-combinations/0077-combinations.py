752fd373ee1b9ca235643fed5e1657564e07ffa9
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-03-11 14:46:01 +0500

    Time: 724 ms (50.51%), Space: 64.7 MB (65.87%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,22 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combs = []
-        self.helper(1, [], combs, n, k)
-        return combs
-    
-    def helper(self, i, curCombs, combs, n, k):
-        if len(curCombs) == k:
-            combs.append(curCombs.copy())
-            return 
         
-        if i > n:
-            return
-        
-        for j in range(i, n + 1):
-            curCombs.append(j)
-            self.helper(j + 1, curCombs, combs, n, k)
+        def helper(i, curCombs, combs):
+            if len(curCombs) >= k:
+                combs.append(curCombs.copy())
+                return None
+            
+            if i > n:
+                return None
+            
+            curCombs.append(i)
+            helper(i + 1, curCombs, combs)
             curCombs.pop()
+            
+            helper(i + 1, curCombs, combs)
+            
+            curCombs
+        
+        helper(1, [], combs)
+        return combs
