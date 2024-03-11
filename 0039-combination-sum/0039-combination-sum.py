ccbb4109b6d4693305b88a34c77802f9c7fca366
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-03-11 14:52:06 +0500

    Time: 62 ms (23.71%), Space: 16.5 MB (87.80%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         combs = []
-        self.helper(0, candidates, [], combs, target)
+        
+        def helper(i, curCombs, combs):
+            sumVal = sum(curCombs)
+            if sumVal == target:
+                combs.append(curCombs.copy())
+                return None
+            
+            if sumVal > target or i >= len(candidates):
+                return None
+            
+            curCombs.append(candidates[i])
+            helper(i, curCombs,combs)
+            curCombs.pop()
+            
+            helper(i + 1, curCombs,combs)
+            
+            
+        helper(0, [], combs)
         return combs
-    
-    
-    def helper(self, i, nums, curCombs, combs, target):
-        curSum = sum(curCombs)
-        if curSum > target or i >= len(nums):
-            return
-        if curSum == target:
-            combs.append(curCombs.copy())
-            return
-        
-        curCombs.append(nums[i])
-        self.helper(i, nums, curCombs, combs, target)
-        curCombs.pop()
-        
-        self.helper(i + 1, nums, curCombs, combs, target)
-        
-        
