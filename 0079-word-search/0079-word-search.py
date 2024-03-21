6b4397395ee5c7bd8ff184b2d17b2e4741587506
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-03-21 11:14:52 +0500

    Time: 7448 ms (6.75%), Space: 16.5 MB (95.39%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,24 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        visited = set()
+        visited = []
         def helper(i, r, c):
-            if i == len(word):
+            if i >= len(word):
+                return
+            
+            if (
+                min(r, c) < 0 or
+                r >= ROWS or c >= COLS or
+                board[r][c] != word[i] or
+                (r, c) in visited
+            ):
+                return
+            
+            if i == len(word) - 1:
                 return True
             
-            if min(r, c) < 0 or r >= ROWS or c >= COLS or (r, c) in visited or word[i] != board[r][c]:
-                return False
-        
-            visited.add((r, c))
-            res = (
-                helper(i + 1, r, c + 1) or
-                helper(i + 1, r, c - 1) or
-                helper(i + 1, r + 1, c) or
-                helper(i + 1, r - 1, c) 
-            )
+            visited.append((r, c))
+            
+            res = (helper(i + 1, r, c + 1) or
+                    helper(i + 1, r, c - 1) or
+                    helper(i + 1, r + 1, c) or
+                    helper(i + 1, r - 1, c))
             visited.remove((r, c))
+            
             return res
-        
+            
         for r in range(ROWS):
             for c in range(COLS):
                 if helper(0, r, c):
