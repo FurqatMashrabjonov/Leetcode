6b073c0a58aea658fe8276fbacc6b598e12935f4
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-04-15 14:55:52 +0500

    Time: 1172 ms (15.59%), Space: 19.2 MB (92.67%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,24 +5,39 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = newNode = ListNode()
+        if len(lists) == 0:
+            return None
+        if len(lists) == 1:
+            return lists[0]
         
-        while True:
-            minIndex = 0
-            minValue = float('inf')
-            for i in range(len(lists)):
-                if lists[i] is None:
-                    continue
-                    
-                if lists[i].val < minValue:
-                    minValue = lists[i].val
-                    minIndex = i
-            if minValue == float('inf'):
-                break
-            tmp = ListNode(minValue)
-            newNode.next = tmp
-            newNode = newNode.next
-            lists[minIndex] = lists[minIndex].next
+        while len(lists) > 1:
+            lists[1] = self.merge(lists[0], lists[1])
+            lists.pop(0)
+        
+        return lists[0]
+        
+        
+        
+        
+        
+    
+    
+    def merge(self, list1, list2):
+        dummy = list3 = ListNode()
+        #TODO: optimize me
+        while list1 and list2:
+            if list1.val > list2.val:
+                list3.next = list2
+                list2 = list2.next
+            else:
+                list3.next = list1
+                list1 = list1.next
+            
+            list3 = list3.next
+        
+        
+        list3.next = list1 if list1 else list2
         
         return dummy.next
+        
         
