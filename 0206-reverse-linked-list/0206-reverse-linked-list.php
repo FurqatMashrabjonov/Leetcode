0e7a8da4dd6ab4c4afb03356b25f0027b63381df
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-05-29 21:55:43 +0500

    Time: 13 ms (14.69%), Space: 21.1 MB (87.57%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.php
@@ -0,0 +1,30 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function reverseList($head) {
+        $cur = $head;
+        $prev = null;
+        while ($cur) {
+            $next = $cur->next;
+            $cur->next = $prev;
+            $prev = $cur;
+            $cur = $next;
+        }
+        
+        return $prev;
+    }
+}
