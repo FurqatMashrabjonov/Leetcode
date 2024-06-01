cbdc1d02308b3291cc9ccf87c6baee453e8bf1b9
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-06-01 23:37:57 +0500

    Time: 36 ms (60.47%), Space: 16.6 MB (34.29%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,10 +1,8 @@
 class Solution:
-    def getPos(self, c):
-        return ord(c) - 64
     
     def titleToNumber(self, col: str) -> int:
         res = 0
         for i in range(len(col)):
-            res += self.getPos(col[i]) * pow(26, len(col) - i - 1)
+            res += (ord(col[i]) - 64) * pow(26, len(col) - i - 1)
         
         return res
