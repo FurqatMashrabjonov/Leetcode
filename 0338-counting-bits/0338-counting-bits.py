4620e9b361f69e6095d8f4a047abaf171e9dcb5b
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-06-14 19:17:48 +0500

    Time: 121 ms (11.52%), Space: 23 MB (94.09%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,19 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res = []
+        ans = []
         cache = {}
         for num in range(n + 1):
             count = 0
-            while num:
-                if num in cache:
-                    count += cache[num]
-                    break
-                    
+            while num > 0:
                 if num & 1 == 1:
                     count += 1
-                
-                num = num >> 1
-            cache[num] = count
-            res.append(count)
+                num >>= 1
+            ans.append(count)
         
-        return res
+        
+        return ans
