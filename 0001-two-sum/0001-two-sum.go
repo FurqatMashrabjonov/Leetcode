967b273633995fe457710a2a20fd3771bf95b27b
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-06-28 22:52:18 +0500

    Time: 6 ms (68.73%), Space: 4.3 MB (11.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    for i, _ := range nums {
-        for j := i + 1; j < len(nums);j += 1 {
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            }
+    hash := make(map[int]int)
+    
+    for i, v := range nums {
+        if val, ok := hash[v]; ok {
+            return []int{val, i}
         }
+        hash[target - v] = i
     }
+    
+    
     return []int{}
 }
